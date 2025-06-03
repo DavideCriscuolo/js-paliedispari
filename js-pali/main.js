b49af53c8d6acca415e887d593154b28d03f1373
DavideCriscuolo <davide.alcatel@gmail.com>
2025-06-03 14:42:26 +0200

    aggiunta funzione per verificare lo stato palindromo di una parola

--- js-pali/main.js
+++ js-pali/main.js
@@ -2,18 +2,23 @@
 //Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
 // mi creo delle variabili una per il prompt e una di prova
-const userWord = prompt("Inserisci una parola");
-const word = "davide";
-let reverseword = "";
-//creo il cilco for per  rovesciare le lettere
-for (let i = word.length - 1; i >= 0; i--) {
-  const wordChar = word[i];
-  reverseword = reverseword + wordChar;
-  if (reverseword === word) {
-    console.log("è palindorma");
-  } else {
-    console.log("non è palindorma");
-  }
-}
+// const userWord = prompt("Inserisci una parola");
 
-function verifyPali() {}
+//creo il cilco for per  rovesciare le lettere
+let userWord = prompt("Inserisci una parola");
+
+function verifyPali(word) {
+  let reverseword = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    const wordChar = word[i];
+
+    reverseword = reverseword + wordChar;
+  }
+  if (reverseword === word) {
+    console.log(`La parola ${word} che hai scelto è palindroma`);
+  } else {
+    console.log(`La parola ${word} che hai scelto non è palindroma`);
+  }
+  return reverseword;
+}
+verifyPali(userWord);
